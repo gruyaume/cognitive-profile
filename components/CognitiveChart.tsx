6290c111de592d4a51efb38082116c1df396c654
Guillaume Belanger <guillaume.belanger@canonical.com>
2023-10-18 08:52:47 -0400

    feat: Uses modern colors

--- components/CognitiveChart.tsx
+++ components/CognitiveChart.tsx
@@ -1,114 +1,105 @@
 import React from "react";
 import Chart from "chart.js/auto";
 import Annotation from "chartjs-plugin-annotation";
+import { ChartConfiguration } from "chart.js";
 
-Chart.register(Annotation);
-Chart.register({
-  id: "customCanvasBackgroundColor",
-  beforeDraw: (chart, args, options) => {
-    const ctx = chart.canvas.getContext("2d");
-    const yScale = chart.scales.y;
+const COLORS = [
+  "#ff6384",
+  "#ff9f40",
+  "#ffcd56",
+  "#4bc0c0",
+  "#36a2eb",
+  "#9966ff",
+  "#ff80b3",
+];
+const SEPARATIONS = [0, 2, 9, 25, 75, 91, 98, 100];
 
-    const colors = options.colors;
-    const separations = options.separations;
+type ChartData = {
+  [functionName: string]: {
+    [indexName: string]: number;
+  };
+};
 
-    for (let i = 0; i < separations.length - 1; i++) {
-      const start = yScale.getPixelForValue(separations[i]);
-      const end = yScale.getPixelForValue(separations[i + 1]);
-      ctx.fillStyle = colors[i];
-      ctx.fillRect(0, start, chart.width, end - start);
-    }
-  },
-});
+Chart.register(
+  Annotation,
+  {
+    id: "customCanvasBackgroundColor",
+    beforeDraw: (chart, args, options) => {
+      const ctx = chart.canvas.getContext("2d");
+      if (!ctx) return;
 
-Chart.register({
-  id: "drawVerticalLines",
-  afterDraw: (chart, args, options) => {
-    const ctx = chart.ctx;
-    const xAxis = chart.scales.x;
-    const separatorIndices = options.separatorIndices;
+      const yScale = chart.scales.y;
 
-    const yOffset = chart.chartArea.bottom + 250;
+      const colors = options.colors;
+      const separations = options.separations;
 
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 2;
-
-    if (separatorIndices) {
-      separatorIndices.forEach((index) => {
-        const xPos = xAxis.getPixelForValue(index);
-        ctx.beginPath();
-        ctx.moveTo(xPos, chart.chartArea.top);
-        ctx.lineTo(xPos, yOffset);
-        ctx.stroke();
-      });
-    }
-  },
-});
-
-Chart.register({
-  id: "customFunctionLabelDrawing",
-  afterDraw: (chart, args, options) => {
-    const ctx = chart.ctx;
-    const xAxis = chart.scales.x;
-    const chartData = options.data;
-    const allIndices = options.allIndices;
-    let lastSeparatorIndex = 0;
-
-    ctx.fillStyle = "black";
-    ctx.textAlign = "center";
-    ctx.font = "bold 16px Arial";
-
-    const yOffset = chart.chartArea.bottom + 250;
-
-    for (const [functionName, functionData] of Object.entries(chartData)) {
-      const functionLength = Object.keys(functionData).length;
-      const startingLabel = allIndices[lastSeparatorIndex];
-      const endingLabel = allIndices[lastSeparatorIndex + functionLength - 1];
-
-      const startingPosition = xAxis.getPixelForValue(startingLabel);
-      const endingPosition = xAxis.getPixelForValue(endingLabel);
-      const centerPosition = (startingPosition + endingPosition) / 2;
-
-      ctx.fillText(functionName, centerPosition, yOffset);
-
-      lastSeparatorIndex += functionLength;
-    }
-  },
-});
-
-Chart.register({
-  id: "drawGaussianCurve",
-  beforeDraw: (chart) => {
-    const ctx = chart.ctx;
-    const yScale = chart.scales.y;
-
-    ctx.strokeStyle = "gray";
-    ctx.lineWidth = 2;
-
-    const mean = 50;
-    const stdDev = 15;
-    const scaleFactor = 20;
-
-    const gaussian = (x) => {
-      return (
-        Math.exp(-0.5 * Math.pow((x - mean) / stdDev, 2)) /
-        (stdDev * Math.sqrt(2 * Math.PI))
-      );
-    };
-
-    ctx.beginPath();
-    for (let i = 0; i <= 100; i++) {
-      const y = i;
-      const x = gaussian(i) * scaleFactor;
-      if (i === 0) {
-        ctx.moveTo(x, yScale.getPixelForValue(y));
-      } else {
-        ctx.lineTo(x, yScale.getPixelForValue(y));
+      for (let i = 0; i < separations.length - 1; i++) {
+        const start = yScale.getPixelForValue(separations[i]);
+        const end = yScale.getPixelForValue(separations[i + 1]);
+        ctx.fillStyle = colors[i];
+        ctx.fillRect(
+          chart.chartArea.left,
+          start,
+          chart.chartArea.width,
+          end - start
+        );
       }
-    }
-    ctx.stroke();
+    },
   },
-});
+  {
+    id: "drawVerticalLines",
+    afterDraw: (chart, args, options) => {
+      const ctx = chart.ctx;
+      const xAxis = chart.scales.x;
+      const separatorIndices = options.separatorIndices;
+
+      const yOffset = chart.chartArea.bottom + 280;
+
+      ctx.strokeStyle = "grey";
+      ctx.lineWidth = 2;
+
+      if (separatorIndices) {
+        separatorIndices.forEach((index: number) => {
+          const xPos = xAxis.getPixelForValue(index);
+          ctx.beginPath();
+          ctx.moveTo(xPos, chart.chartArea.top);
+          ctx.lineTo(xPos, yOffset);
+          ctx.stroke();
+        });
+      }
+    },
+  },
+  {
+    id: "customFunctionLabelDrawing",
+    afterDraw: (chart, args, options) => {
+      const ctx = chart.ctx;
+      const xAxis = chart.scales.x;
+      const chartData: ChartData = options.data;
+      const allIndices = options.allIndices;
+      let lastSeparatorIndex = 0;
+
+      ctx.fillStyle = "grey";
+      ctx.textAlign = "center";
+      ctx.font = "bold 24px Arial";
+
+      const yOffset = chart.chartArea.bottom + 280;
+
+      for (const [functionName, functionData] of Object.entries(chartData)) {
+        const functionLength = Object.keys(functionData).length;
+        const startingLabel = allIndices[lastSeparatorIndex];
+        const endingLabel = allIndices[lastSeparatorIndex + functionLength - 1];
+
+        const startingPosition = xAxis.getPixelForValue(startingLabel);
+        const endingPosition = xAxis.getPixelForValue(endingLabel);
+        const centerPosition = (startingPosition + endingPosition) / 2;
+
+        ctx.fillText(functionName, centerPosition, yOffset);
+
+        lastSeparatorIndex += functionLength;
+      }
+    },
+  }
+);
 
 interface ResultMapping {
   [functionName: string]: {
@@ -120,7 +111,7 @@ interface CognitiveChartProps {
   data: ResultMapping;
 }
 
-const CognitiveChart: React.FC<CognitiveChartProps> = ({ data }) => {
+const populateDataArrays = (data: ChartData) => {
   const allIndices: string[] = [];
   const percentiles: number[] = [];
   const separatorIndices: number[] = [];
@@ -134,9 +125,14 @@ const CognitiveChart: React.FC<CognitiveChartProps> = ({ data }) => {
       percentiles.push(data[functionName][index]);
     }
   }
+  return { allIndices, percentiles, separatorIndices };
+};
 
-  const colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
-  const separations = [0, 2, 9, 25, 75, 91, 98, 100];
+const CognitiveChart: React.FC<CognitiveChartProps> = ({ data }) => {
+  const { allIndices, percentiles, separatorIndices } = React.useMemo(
+    () => populateDataArrays(data),
+    [data]
+  );
 
   const config = {
     type: "line",
@@ -162,19 +158,27 @@ const CognitiveChart: React.FC<CognitiveChartProps> = ({ data }) => {
         y: {
           min: 0,
           max: 100,
+          ticks: {
+            font: {
+              size: 16,
+            },
+          },
         },
         x: {
           ticks: {
             autoSkip: false,
             maxRotation: 90,
             minRotation: 90,
+            font: {
+              size: 16,
+            },
           },
         },
       },
       plugins: {
         customCanvasBackgroundColor: {
-          colors: colors,
-          separations: separations,
+          colors: COLORS,
+          separations: SEPARATIONS,
         },
         customFunctionLabelDrawing: {
           data: data,
@@ -199,7 +203,7 @@ const CognitiveChart: React.FC<CognitiveChartProps> = ({ data }) => {
         },
       },
     },
-  };
+  } as ChartConfiguration;
 
   return (
     <canvas
